6b609f1f9433342e697973b8567603155ecde098
black-turleneck <49991958+black-turleneck@users.noreply.github.com>
2019-05-31 19:37:03 +1000

    Update MyClassifier.java
    Fixed kNN
    Has print statements for testing

--- MyClassifier.java
+++ MyClassifier.java
@@ -2,20 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class MyClassifier {
-	// declare global vars
-	public static File training_file;
-	public static File testing_file;
-	public static Scanner scan_tr;
-	public static Scanner scan_test;
-	public static ArrayList<double[]> training_data;
-	public static ArrayList<double[]> testing_data;
 
 	public static void main(String[] args) {
+		File training_file;
+		File testing_file;
+		Scanner scan_tr;
+		Scanner scan_test;
+		ArrayList<double[]> training_list;
+		ArrayList<double[]> testing_list;		
+		
+	
 		// check num args
-		System.out.println("*num args: " + args.length + "*");
 		if (args.length != 3) {
 			// do sth
-			System.out.println("Too many/little args");
+			System.out.println("*Too many/little args" + args.length);
 			return;
 		}
 
@@ -26,71 +26,40 @@ public class MyClassifier {
 		String testing_path = args[1];
 		String algo = args[2];
 
+		// open files and scanners
 		try {
-			// open files and scanners
 			training_file = new File(training_path);
 			testing_file = new File(testing_path);
 			scan_tr = new Scanner(training_file);
 			scan_test = new Scanner(testing_file);
-			training_data = new ArrayList<double[]>();
-
-			// add all examples from file into TRAINING_data list
-			while (scan_tr.hasNextLine()) {
-				String line = scan_tr.nextLine();
-				String[] entry_str = line.replaceAll("ï»¿","").split(",");
-				// change class vars to non-numeric
-				if (entry_str[entry_str.length - 1].contentEquals("yes")) {
-					entry_str[entry_str.length - 1] = Integer.toString(1);
-				} else {
-					// test for case where isn't no?
-					entry_str[entry_str.length - 1] = Integer.toString(0);
-				}
-				//testing
-				//System.out.println("String form: " + Arrays.toString(entry_str));
-				// change everything to number
-				double[] entry = new double[entry_str.length];
-				for (int i = 0; i < entry_str.length; i++) {
-					double sth = Double.parseDouble(entry_str[i]);
-					entry[i] = sth;			
-				}
-				System.out.println("Digit form (training file): " + Arrays.toString(entry));
-				training_data.add(entry);
-				// other break condition?
-			}
-
-			
-		} catch (FileNotFoundException e) {
-			// return;
+		} 
+		catch (FileNotFoundException e) {
 			e.printStackTrace();
+			return;
 		}
 
+		training_list = makeTrainingList(scan_tr);
+		testing_list = makeTestingList(scan_test);	
+			
+		//implement one of the 2 classifiers
 		if (algo.equals("NB")) {
 			naiveBayes();
-		} else
-			try {
-				// check if fits kNN format
-				// have accounted for wrong String length (?)
-				//System.out.println(algo);
-				String[] split_str = algo.split("");
-				System.out.println("The String array for kNN is: " + Arrays.toString(split_str));
-				int num_neighbours = Integer.parseInt(split_str[0]);
-
-				if (split_str[1].equals("N") && split_str[2].equals("N")) {
-					kNearestNeighbour(num_neighbours);
-				}
-
-			} catch (Exception exc) {
-				exc.printStackTrace();
-			}
-
+		} 
+		else if (get_k(algo) != 0) {
+			kNearestNeighbour(get_k(algo), training_list, testing_list);
+		}
+		else {
+			System.out.println("*Something went wrong: kNN format*");
+		}
 		System.out.println("*End of program*");
 	}
+	//end of Main method
+	//supporting methods:
+	
 
-	private static void naiveBayes() {
-		System.out.println("*Got to Naive Bayes*");
-	}
-
-	private static void kNearestNeighbour(int num_neighbours) {
+	private static void kNearestNeighbour(int k, ArrayList<double[]> training, ArrayList<double[]> testing) {
+		
+		System.out.println("Starting kNN with k = " + k);
 		// vars: Num, lists
 		// for each example in testing list
 		// calculate Euclidean distance to each training example 
@@ -98,105 +67,122 @@ public class MyClassifier {
 		// look at class of N selected training examples
 		// majority vote on class of testing example
 		
-		// go through all examples from file into TESTING_data list
-		
-		ArrayList<double[]> n_closest = new ArrayList<double[]>();
-		//go through testing examples
-		while (scan_test.hasNextLine()) {
-
-			String line = scan_test.nextLine();
-			String[] entry_str = line.replaceAll("ï»¿","").split(",");
-
-			// change everything to number
-			double[] test_arr = new double[entry_str.length];
-			for (int i = 0; i < entry_str.length; i++) {
-				double sth = Double.parseDouble(entry_str[i]);
-				test_arr[i] = sth;			
-				System.out.println(sth);
-			}
+		// go through all TESTING examples
+		for(double[] testing_ex : testing) {
+			//this arraylist stores arrays of doubles, of the form [distance, class], with class 0 or 1
+			ArrayList<double[]> k_nearest = new ArrayList<double[]>();
+			System.out.println("New test");
 			
-			//for each test patient, go through all training examples
+			//for each test line, go through all TRAINING examples
 			int n = 0;
-			System.out.println("Calculating distance from test to all training ex");
-			for (double[] train : training_data) {
-				//make new instance for each training example
-				//each has distance to test and class of training example
-				double distance = dist_Euclidean(test_arr,train);
-				double[] training_ex = {distance, train[train.length - 1]};
-				//first k examples added automatically
-				if(n<num_neighbours) {
-					n_closest.add(training_ex);
+			for (double[] training_ex : training) {
+				//make new [,] instance
+				double distance = dist_Euclidean(testing_ex, training_ex);
+				double classs = training_ex[training_ex.length - 1]; //[training_ex.length - 1] should be 8
+				double[] combined = {distance, classs}; 
+				
+				if (n < k) {
+					k_nearest.add(combined);
+					System.out.println("Adding to nearest: " + Arrays.toString(combined));
 				}
-				//for rest, check if closer to test example 
 				else {
-					int furthest_index = 0;
-					//loop through list of closest to find furthest of those
-					System.out.println("Finding furthest training ex");
-					for(int i = 0; i < num_neighbours; i++) {
-						if(n_closest.get(i)[0] > n_closest.get(furthest_index)[0]) {
-							furthest_index = i;							
-						}	
-					}
-
-					//now compare distance of this furthest one to distance of training example
-					//what to do if the distance is the same for both?? Keep oldest or newest??
-					//!!!
-					System.out.println("Find N closest training ex");
-					if (training_ex[0] < n_closest.get(furthest_index)[0]) {
-						//see if this works with the array instances & stuff
-						//if closer, add this one and delete previous
-						n_closest.remove(n_closest.get(furthest_index));
-						n_closest.add(training_ex);
-					}
+					int furthest = find_furthest_index(k_nearest);
+					if (combined[0] < k_nearest.get(furthest)[0]) {
+						//replaces furthest element in k_nearest by current var
+						System.out.println("Adding to nearest: " + Arrays.toString(combined));
+						System.out.println("Removing from nearest: " + Arrays.toString(k_nearest.get(furthest)));
+						k_nearest.set(furthest, combined);
+						//display length of AList?
+					}					
 				}
+			n++;	
 			}
-			//gone through all training examples
-			//now count amount of yes and no
 			
-			int count = 0;
-			for(double[] arr: n_closest) {
-				if (arr[1] == 1) {
-					count++;
-				}					
-			}	
+			//at this point we have the k nearest training examples to the given test
+			//count num of yes & no
+			int num_yes = 0;
+			int num_no = 0;
+			for (double[] neighbour : k_nearest) {
+				if (neighbour[1] == 1) {
+					num_yes++;
+				}
+				else if (neighbour[1] == 0) {
+					num_no++;
+				}
+				else {
+					System.out.println("Something is wrong with yes/no counting");
+				}				
+			}
 			
-			if(num_neighbours/count > 2) {
-				System.out.println("no");				
-			}	
-			else if (num_neighbours/count <= 2) {
-				System.out.println("yes");
+			//display class yes or no, decided by majority vote (tiebreaker is yes)
+			if (num_yes >= num_no) {
+				System.out.println("yes");				
 			}
 			else {
-				System.out.println("Something is wrong.");
+				System.out.println("no");
 			}			
-		}		
-		//gone through all test examples
+		}
+		System.out.println("*End of kNN*");		
 	}
 
 
+		private static void naiveBayes() {
+			System.out.println("*Got to Naive Bayes*");
+		}
 
-
-
-//		for (double[] test: testing_data) {
-//			//PQ
-//			//a PQ of arrays(?) that store distance, class
-//			PriorityQueue<double[]> n_lowest = new PriorityQueue<double[]>((a,b) -> a[0] - b[0]);
-//			
-//			for (double[] train : training_data) {
-//				double distance = dist_Euclidean(test,train);
-//				if (among N lowest distances) 
-//				//i.e. is lower than all in queue 
-//				//OR there are less than N in queue
-//				{
-//					//add test to n_lowest in defined order					
-//				}
-//				
-//			}
-//		}
-		
-		
 	
 
+	public static ArrayList<double[]> makeTrainingList(Scanner scan_tr) {
+		ArrayList<double[]> training_data = new ArrayList<double[]>();
+		// add all examples from file into TRAINING_data list
+		while (scan_tr.hasNextLine()) {
+			String line = scan_tr.nextLine();
+			String[] entry_str = line.replaceAll("ï»¿","").split(",");
+			// change class vars to non-numeric
+			if (entry_str[entry_str.length - 1].contentEquals("yes")) {
+				entry_str[entry_str.length - 1] = Integer.toString(1);
+			} else {
+				// test for case where isn't no?
+				entry_str[entry_str.length - 1] = Integer.toString(0);
+			}
+			//testing
+			//System.out.println("String form: " + Arrays.toString(entry_str));
+			// change everything to number
+			double[] entry = new double[entry_str.length];
+			for (int i = 0; i < entry_str.length; i++) {
+				double sth = Double.parseDouble(entry_str[i]);
+				entry[i] = sth;			
+			}
+			System.out.println("Digit form (training file): " + Arrays.toString(entry));
+			training_data.add(entry);
+		}
+		return training_data;
+	}
+	
+	public static ArrayList<double[]> makeTestingList(Scanner scan_test) {
+		ArrayList<double[]> testing_data = new ArrayList<double[]>();
+		// add all examples from file into TRAINING_data list
+		
+		while (scan_test.hasNextLine()) {
+			//read in line, convert to String array
+			String line = scan_test.nextLine();
+			String[] entry_str = line.replaceAll("ï»¿","").split(",");
+			
+			//System.out.println("String form: " + Arrays.toString(entry_str));
+			// convert to number
+			double[] entry = new double[entry_str.length];			
+			for (int i = 0; i < entry_str.length; i++) {
+				double sth = Double.parseDouble(entry_str[i]);
+				entry[i] = sth;			
+			}
+			
+			System.out.println("Digit form (testing file): " + Arrays.toString(entry));
+			testing_data.add(entry);
+		}
+		return testing_data;
+	}
+
+	
 	private static double dist_Euclidean(double[] a, double[] b) {
 		// for first 8 attributes:
 		double dist = 0;
@@ -211,5 +197,37 @@ public class MyClassifier {
 		
 		return dist;
 	}
+	
+	
+	// check if fits kNN format
+	// have accounted for wrong String length?
+	private static int get_k(String algo) {
+		try {			
+			//System.out.println(algo);
+			String[] split_str = algo.split("");
+			int num_neighbours = Integer.parseInt(split_str[0]);
 
+			if (split_str[1].equals("N") && split_str[2].equals("N")) {
+				return num_neighbours;
+			}
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		//doesn't fit format
+		return 0;
+	}
+	
+	private static int find_furthest_index(ArrayList<double[]> k_nearest) {
+		int furthest_index = 0;
+		int curr_index = 0;
+		for (double[] neighbour : k_nearest) {
+			if(neighbour[0] > k_nearest.get(furthest_index)[0]) {
+				furthest_index = curr_index;							
+			}	
+			curr_index++;
+		}		
+		return furthest_index;
+	}
+	
+	
 }
