ae4ee58f7e4e08f231b4400a557b8b0027a633c2
black-turleneck <49991958+black-turleneck@users.noreply.github.com>
2019-06-01 05:18:34 +1000

    Update MyClassifier.java
    Started implementing Naive Bayes, have to fix a couple of things like deal with stdev = 0 case
    Has comments for testing

--- MyClassifier.java
+++ MyClassifier.java
@@ -1,3 +1,6 @@
+//if standard deviation is  0, set prob dens fctn to 1!!!
+
+
 import java.io.*;
 import java.util.*;
 
@@ -43,7 +46,7 @@ public class MyClassifier {
 			
 		//implement one of the 2 classifiers
 		if (algo.equals("NB")) {
-			naiveBayes();
+			naiveBayes(training_list, testing_list);
 		} 
 		else if (get_k(algo) != 0) {
 			kNearestNeighbour(get_k(algo), training_list, testing_list);
@@ -126,11 +129,157 @@ public class MyClassifier {
 	}
 
 
-		private static void naiveBayes() {
-			System.out.println("*Got to Naive Bayes*");
+	private static void naiveBayes(ArrayList<double[]> training, ArrayList<double[]> testing) {
+		//find mean and stdev for each attribute (1 to 8 --i.e. 0 to 7), for yes & no respectively		
+		ArrayList<double[]> stats = getStats(training);
+		double[] meanyes = stats.get(0);
+		double[] meanno = stats.get(1);
+		double[] sdyes = stats.get(2);
+		double[] sdno = stats.get(3);
+		double n_yes = stats.get(4)[0];
+		double n_no = stats.get(4)[1];
+		double n_total = n_yes + n_no;
+		//check if these 2 are right!
+		double probyes = n_yes/n_total;
+		double probno = n_no/n_total;
+		
+		double yes_fctn = 1;
+		double no_fctn = 1;	
+		
+		double var_test = 0;
+		double meanyes_test = 0;
+		double sdyes_test = 0;
+		
+		//for each test, compute Bayes method maths with prob dens function
+		for (double[] testing_ex : testing) {
+			//wrong!! there should be a diff prob function for each index (attribute)
+			for (int i = 0; i < 8; i++) {
+			yes_fctn = yes_fctn*probFunction(testing_ex[i], meanyes[i], sdyes[i]);
+			no_fctn = no_fctn*probFunction(testing_ex[i], meanno[i], sdno[i]);
+			//System.out.println("wat??"); 
+			//System.out.println(testing_ex[i]);
+			//System.out.println(meanyes[i]);
+			//System.out.println(sdyes[i]);
+			if (i == 0) {
+				var_test =testing_ex[i];
+				meanyes_test = meanyes[i];
+				sdyes_test = sdyes[i];
+			}
+			}
+			yes_fctn = yes_fctn*probyes;
+			no_fctn = no_fctn*probno;
+			
+			System.out.println(yes_fctn);
+			System.out.println(no_fctn);
+			if (yes_fctn >= no_fctn) {
+				System.out.println("yes");
+			}
+			else if (no_fctn > yes_fctn) {
+				System.out.println("no");
+			}
+			else {
+				System.out.println("Something is wrong with yes/no choosing");
+			}
+		}
+		//System.out.println("End of NB. Starting test...");
+		//System.out.println(var_test + meanyes_test + sdyes_test);
+		//System.out.println(probFunction(var_test, meanyes_test, sdyes_test));
+		
+	}	
+
+	public static double probFunction(double val, double mean, double std) {
+		double function = 0;
+		double sub_fctn = -Math.pow(val-mean,2)/(2*Math.pow(std, 2));
+		function = 1/(std*Math.sqrt(2*Math.PI))*Math.pow(Math.E,sub_fctn);
+		//System.out.println("Calculated function is: " + function);
+		return function;
+	}
+	
+	
+	public static ArrayList<double[]> getStats(ArrayList<double[]> training) {
+		ArrayList<double[]> stats = new ArrayList<double[]>();
+		double[] sumyes = new double[8];
+		double[] sumno = new double[8];
+		double[] meanyes = new double[8];
+		double[] meanno = new double[8];
+		double[] diffyes = new double[8];
+		double[] diffno = new double[8];
+		double[] sdyes = new double[8];
+		double[] sdno = new double[8];
+		double n_yes = 0;
+		double n_no = 0;
+		double[] nums = {n_yes, n_no};
+		for (double[] training_ex : training) {
+			//if example is class yes
+			if (training_ex[8] == 1) {
+				for (int i = 0; i < 8; i++) {
+					//do I have to initialise this to all 0 for sum?
+					sumyes[i] += training_ex[i];
+				}
+				n_yes++;
+			} 
+			//if class no
+			else if (training_ex[8] == 0) { 
+				for (int i = 0; i < 8; i++) {
+					sumno[i] += training_ex[i];
+				}
+				n_no++;
+			}
+			else {
+				System.out.println("Sometihng went wrong: Not class yes or no");
+			}				
 		}
 
-	
+		//divide each index by n
+		for (int i = 0; i < 8; i++) {
+			meanyes[i] = sumyes[i] / n_yes;
+			meanno[i] = sumno[i] / n_no;
+			//do some testing
+		}
+
+		System.out.println("Calc stdev");
+		//standard deviation
+		for (double[] training_ex : training) {
+			//if example is class yes
+			if (training_ex[8] == 1) {
+				for (int i = 0; i < 8; i++) {
+					diffyes[i] += Math.sqrt(training_ex[i] - meanyes[i]); 
+					System.out.println("For yes: training_ex[i], meanyes[i], diffyes[i] is: ");
+					System.out.println(training_ex[i]);
+					System.out.println(meanyes[i]);
+					System.out.println(diffyes[i]);
+				}
+			}
+			//if class no
+			else if (training_ex[8] == 0) {
+				for (int i = 0; i < 8; i++) {
+					diffno[i] = diffno[i] + Math.sqrt(training_ex[i] - meanno[i]); 
+				}
+			}
+			else {
+				System.out.println("Something went wrong: Not class yes or no");
+			}				
+			System.out.println(Arrays.toString(diffyes));
+		}
+
+		for (int i = 0; i < 8; i++) {
+			sdyes[i] = diffyes[i] / (n_yes - 1);
+			sdno[i] = Math.sqrt(diffno[i] / (n_no - 1));
+			//do some testing
+		}
+		
+		
+		nums[0] = n_yes;
+		nums[1] = n_no;
+		
+		stats.add(meanyes);
+		stats.add(meanno);
+		stats.add(sdyes);
+		stats.add(sdno);
+		stats.add(nums);
+		return stats;	
+	}
+
 
 	public static ArrayList<double[]> makeTrainingList(Scanner scan_tr) {
 		ArrayList<double[]> training_data = new ArrayList<double[]>();
@@ -229,7 +378,6 @@ public class MyClassifier {
 			curr_index++;
 		}		
 		return furthest_index;
-	}
-	
+	}	
 	
 }
